b92b7467df9c0fc62112b8ec908d86a41dadcde2
tmkimm <xoals1417@gmail.com>
2020-12-29 00:11:22 +0900

    Remove jade engine

--- server/app.js
+++ server/app.js
@@ -8,11 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
