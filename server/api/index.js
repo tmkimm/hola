33ebda5172a2e1bea1b13957dcfe3435dad234f6
tmkimm <xoals1417@gmail.com>
2021-03-01 22:59:27 +0900

    Oauth2.0 Google 로그인 기능 추가(미완료)

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import study from './routes/study.js';
+import auth from './routes/auth.js';
 
 export default () => {
     const app = Router();
     study(app);
+    auth(app);
 
     return app;
 }
