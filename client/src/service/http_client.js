5cf5d1f5fc05d8b50260fe15446c63c24e004873
Sihyun <sihyun930222@gmail.com>
2021-04-16 10:15:29 +0900

    fix : opacity 버그 수정
    바뀌지 않아 두번째 수정

--- client/src/service/http_client.js
+++ client/src/service/http_client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_DEV = "http://localhost:5000/api/";
-const API_PRODUCT = "https://holaworld-api.tk";
+const API_PRODUCT = "https://holaworld-api.tk/";
 const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 
 const httpClient = axios.create({
