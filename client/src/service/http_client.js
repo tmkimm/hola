0b92363aeb9a1cca6e15f3a06e3e4417b05ea3ef
Sihyun <sihyun930222@gmail.com>
2021-04-16 09:27:06 +0900

    fix : API 주소 변경
    https 인증서를 적용한 도메인으로 백엔드 API 주소를 변경하였습니다.

--- client/src/service/http_client.js
+++ client/src/service/http_client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API_DEV = "http://localhost:5000/api/";
-const API_PRODUCT = "";
-let baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
+const API_PRODUCT = "https://holaworld-api.tk";
+const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 
 const httpClient = axios.create({
   baseURL,
