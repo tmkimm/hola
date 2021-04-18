a6633e96c2f0a7688ef530ace6843e1b17f2d82f
Sihyun <sihyun930222@gmail.com>
2021-04-18 14:44:11 +0900

    fix: api 서버 주소 수정
    ssl 인증서 이슈가 있어 api 서버 주소를 수정합니다.

--- client/src/service/http_client.js
+++ client/src/service/http_client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_DEV = "http://localhost:5000/api/";
-const API_PRODUCT = "https://holaworld-api.tk/api/";
+const API_PRODUCT = "https://www.holaworld-api.tk/api/";
 const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 
 const httpClient = axios.create({
