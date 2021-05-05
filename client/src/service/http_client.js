cba40912e8a326c7df24802efbf87850eec34181
tmkimm <xoals1417@gmail.com>
2021-05-05 23:51:43 +0900

    feat: 마이페이지 UI 추가

--- client/src/service/http_client.js
+++ client/src/service/http_client.js
@@ -4,8 +4,8 @@ const API_DEV = "http://localhost:5000/api/";
 const API_PRODUCT = "https://www.holaworld-api.tk/api/";
 const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 
+axios.defaults.withCredentials = true;
 const httpClient = axios.create({
   baseURL,
 });
-
 export default httpClient;
