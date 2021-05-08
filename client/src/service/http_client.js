91574867c00fe09a2817ca1307828caff01dd0e6
tmkimm <xoals1417@gmail.com>
2021-05-08 12:14:35 +0900

    fix: CORS 이슈 해결
    Refresh Token이 설정되지 않는 이슈가 수정되었습니다.
    CORS를 Whitelist로 관리하도록 수정되었습니다.

--- client/src/service/http_client.js
+++ client/src/service/http_client.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API_DEV = "http://localhost:5000/api/";
-const API_PRODUCT = "https://www.holaworld-api.tk/api/";
+const API_PRODUCT = "https://holaworld-api.com/api/";
 const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 
-axios.defaults.withCredentials = true;
 const httpClient = axios.create({
   baseURL,
+  withCredentials:true,
 });
 export default httpClient;
