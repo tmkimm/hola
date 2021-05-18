95dae463d4fe389b1c85ec8a492c0d65f1ea0b64
tmkimm <xoals1417@gmail.com>
2021-05-18 23:45:26 +0900

    fix: 글 등록 필수입력 필드 수정

--- server/api/middlewares/isStudyValid.js
+++ server/api/middlewares/isStudyValid.js
@@ -1,18 +1,19 @@
 import {body, validationResult} from 'express-validator';
 
 const checkStudy = [
-  body('topic').isString().withMessage('Invaild datatype(String)'),
-  body('language').isArray().withMessage('Invaild datatype(Array)'),
-  body('location').isString().withMessage('Invaild datatype(String)'),
-  body('position').isArray().withMessage('Invaild datatype(Array)'),
-  body('position.*.part').isString().withMessage('Invaild datatype(String)'),
-  body('position.*.personnel').isNumeric().withMessage('Invaild datatype(Number)'),
+  // body('topic').isString().withMessage('Invaild datatype(String)'),
+  // body('language').isArray().withMessage('Invaild datatype(Array)'),
+  // body('location').isString().withMessage('Invaild datatype(String)'),
+  // body('position').isArray().withMessage('Invaild datatype(Array)'),
+  // body('position.*.part').isString().withMessage('Invaild datatype(String)'),
+  // body('position.*.personnel').isNumeric().withMessage('Invaild datatype(Number)'),
   body('title').isString().withMessage('Invaild datatype(String)'),
   body('content').isString().withMessage('Invaild datatype(String)')
 ];
 
 // Study 유효성 검증 미들웨어
 const isStudyValid = (req, res, next) => {
+  console.log(req.body);
   const errors = validationResult(req);
   if(!errors.isEmpty())
     return res.status(400).json({ errors: errors.array() });  
