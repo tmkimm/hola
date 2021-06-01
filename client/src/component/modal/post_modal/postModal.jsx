17a366e7874882c230fc72e1024309e4b6d3f070
Sihyun <sihyun930222@gmail.com>
2021-06-01 23:31:31 +0900

    feat: postmodal UI 수정 & test button 추가
    postmodal component를 수정하였습니다.

--- client/src/component/modal/post_modal/postModal.jsx
+++ client/src/component/modal/post_modal/postModal.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import studyService from "../../../service/study_service";
 import styles from "./postModal.module.css";
 
@@ -13,23 +14,44 @@ To-Do
 2. min-height 어떻게 처리할지 구현
 
 */
-const PostModal = ({ study, handleClose }) => {
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
 
+const handleEdit = () => {
+  console.log("test edit");
+};
+
+const handleDelete = () => {
+  console.log("test delete");
+};
+
+const TestButton = () => {
+  return (
+    <section className={styles.buttonWrapper}>
+      <button onClick={handleEdit}>수정</button>
+      <button onClick={handleDelete}>삭제</button>
+    </section>
+  );
+};
+
+const PostModal = ({ study, handleClose }) => {
+  const user = useSelector((state) => state.user);
+  const [content, setContent] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [imagePath, setImagePath] = useState("");
+  console.log("###nickname : ", user.nickName);
   useEffect(() => {
     studyService.getDetail(study._id).then((response) => {
       setContent((state) => response.data.content);
-      console.log(response);
+      setNickname((state) => response.data.author.nickName);
+      setImagePath((state) => response.data.author.image);
     });
   }, [study._id]);
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.modalHeader}>
+      <section className={styles.modalHeader}>
         <img
           className={styles.logo}
-          src="images/logo/hola_logo_y.png"
+          src="/images/logo/hola_logo_w.png"
           alt="welcome"
         ></img>
         <div className={styles.exitWrapper} onClick={handleClose}>
@@ -46,19 +68,20 @@ const PostModal = ({ study, handleClose }) => {
             <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
           </svg>
         </div>
-      </div>
+      </section>
       <section className={styles.modalContent}>
         <div className={styles.postHeader}>
           <div className={styles.registeredDate}>2021.05.24</div>
-          <div className={styles.title}>파이썬 스터디 구합니다</div>
+          <div className={styles.title}>{study.title}</div>
           <div className={styles.user}>
             <img
               className={styles.userImg}
               src="https://media.vlpt.us/images/seeh_h/profile/6b7bfde5-b67c-4665-a2e1-a308e8de2059/tt.PNG?w=120"
               alt="thumbnail"
             />
-            <div className={styles.userName}>velopert</div>
+            <div className={styles.userName}>{nickname}</div>
           </div>
+          {user.nickName === nickname && <TestButton></TestButton>}
         </div>
         <div className={styles.postContentWrapper}>
           <div
