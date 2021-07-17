dd8f917346c1177ab1e3b1e1501dfd90b7d63eae
Sihyun <sihyun930222@gmail.com>
2021-07-17 22:51:47 +0900

    feat: page 디자인 추가

--- client/src/page/study/study.jsx
+++ client/src/page/study/study.jsx
@@ -4,6 +4,7 @@ import { useHistory, useLocation } from "react-router-dom";
 import CancelButton from "../../component/cancelButton/cancelButton";
 import CommentContainer from "../../component/comment_container/commentContainer";
 import LikesAndViews from "../../component/likes_and_views/likesAndViews";
+import Navbar from "../../component/nav_bar/navbar";
 import { clearPost, readPost } from "../../store/read";
 import { setPost } from "../../store/write";
 import styles from "./study.module.css";
@@ -53,18 +54,12 @@ const Study = () => {
   }, [dispatch, studyId]);
 
   return (
-    <div className={styles.wrapper}>
-      <section className={styles.modalHeader}>
-        <img
-          className={styles.logo}
-          src="/images/logo/hola_logo_y.png"
-          alt="welcome"
-        ></img>
-      </section>
-      <section className={styles.modalContent}>
-        <div className={styles.postHeader}>
-          <div className={styles.registeredDate}>{read.post.createdAt}</div>
+    <>
+      <Navbar showRegisterButton={true}></Navbar>
+      <div className={styles.wrapper}>
+        <section className={styles.postHeader}>
           <div className={styles.title}>{read.post.title}</div>
+          <div className={styles.registeredDate}>{read.post.createdAt}</div>
           <div className={styles.user}>
             <img
               className={styles.userImg}
@@ -80,27 +75,28 @@ const Study = () => {
               post={read.post}
             ></TestButton>
           )}
-        </div>
+        </section>
         <div className={styles.postContentWrapper}>
           <div
             className={styles.postContent}
             dangerouslySetInnerHTML={{ __html: read.post.content }}
           ></div>
         </div>
-      </section>
-      <section className={styles.modalComment}>
-        <LikesAndViews
-          views={read.post.views}
-          likeUser={read.post.likes}
-          likes={read.post.likesCount}
-          studyId={read.post.id}
-          userId={user.id}
-        ></LikesAndViews>
-        <div className={styles.postComment}>
-          <CommentContainer id={read.post.id}></CommentContainer>
-        </div>
-      </section>
-    </div>
+
+        <section className={styles.modalComment}>
+          <LikesAndViews
+            views={read.post.views}
+            likeUser={read.post.likes}
+            likes={read.post.likesCount}
+            studyId={read.post.id}
+            userId={user.id}
+          ></LikesAndViews>
+          <div className={styles.postComment}>
+            <CommentContainer id={studyId}></CommentContainer>
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
