bdf889fff4d8937aef8e7bc3fb6b8afe363a6b1f
tmkimm <xoals1417@gmail.com>
2021-07-23 12:35:45 +0900

    fix: REST API - GET에서 request body를 사용하는 안티패턴 제거
    닉네임 중복 체크 시 request body를 사용하던 부분을 query param을 사용하도록 변경하였습니다.

--- server/api/middlewares/nickNameDuplicationCheck.js
+++ server/api/middlewares/nickNameDuplicationCheck.js
@@ -2,9 +2,10 @@ import { User } from '../../models/User.js';
 
 // 회원 정보 수정 시 닉네임이 중복되었는지 체크한다.
 const nickNameDuplicationCheck = async (req, res, next) => {
+    let { nickName } = req.query;
     try {
-        if(req.body.nickName) {
-            const user = await User.findByNickName(req.body.nickName);
+        if(nickName) {
+            const user = await User.findByNickName(nickName);
             if(user) {
                 return res.status(409).json({
                     message : `Nickname is duplicated.`,
