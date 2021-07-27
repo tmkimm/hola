1becb2a4d5d59533897adb0d4eb98fbc272f3b93
Sihyun <sihyun930222@gmail.com>
2021-07-27 23:20:44 +0900

    fix: api 주소 변경 test

--- client/src/service/http_client.js
+++ client/src/service/http_client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_DEV = "http://localhost:5000/api/";
-const API_PRODUCT = "https://api.holaworld.io/api/";
+const API_PRODUCT = "https://holaworld-api.com/api/";
 const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 const httpClient = axios.create({
   baseURL,
