28e1a45bcca32adc1defd29d229e446e824e145e
Sihyun <sihyun930222@gmail.com>
2021-07-27 23:00:34 +0900

    fix: api 주소 변경
    safari cookie issue로 인해 api 주소를 front-end 배포 domain의 sub-domain으로 변경합니다.

--- client/src/service/http_client.js
+++ client/src/service/http_client.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API_DEV = "http://localhost:5000/api/";
-const API_PRODUCT = "https://holaworld-api.com/api/";
+const API_PRODUCT = "https://api.holaworld.io/api/";
 const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 const httpClient = axios.create({
   baseURL,
-  withCredentials:true,
+  withCredentials: true,
 });
 export default httpClient;
