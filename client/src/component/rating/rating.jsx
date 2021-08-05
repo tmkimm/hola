e9f1132264973ac7ce9b4ef1a60cc5705afee8b9
Sihyun <sihyun930222@gmail.com>
2021-08-05 09:20:34 +0900

    style: rating component 스타일 수정
    평가를 받을 수 있는 rating component 스타일을 수정하였습니다.

--- client/src/component/rating/rating.jsx
+++ client/src/component/rating/rating.jsx
@@ -31,57 +31,63 @@ const Rating = (props) => {
       {showRating ? (
         <section className={styles.container}>
           <header className={styles.header}>
+            <div className={styles.exitWrapper} onClick={handleClick}>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 24 24"
+                tabIndex="1"
+                height="1.5em"
+                width="1.5em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+              </svg>
+            </div>
             <div className={styles.titleWrapper}>
               <div className={styles.title}>Hola에 만족하셨나요?</div>
-              <div className={styles.exitWrapper} onClick={handleClick}>
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 24 24"
-                  tabIndex="1"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-                </svg>
+              <div className={styles.content}>
+                더 좋은 서비스를 위해, 평가를 남겨주세요!
               </div>
             </div>
-            <div className={styles.content}>
-              더 좋은 서비스를 제공해 드릴 수 있도록, 별점과 함께 피드백을
-              남겨주세요!
-            </div>
           </header>
+          <div className={styles.imgWrapper}>
+            <img
+              className={styles.imgLogo}
+              src="/images/logo/hola_default2.png"
+              alt="default logo"
+            />
+          </div>
 
           <div className={styles.ratings}>
             <ReactStars
               isHalf={true}
               count={5}
               onChange={ratingChanged}
-              size={64}
+              size={48}
               activeColor="#ffd700"
             />
           </div>
 
-          <input
-            type="text"
-            name="textbox"
-            placeholder="평가를 입력해주세요..."
-            ref={inputRef}
-          />
-          <footer className={styles.footer}>
+          <div className={styles.inputWrapper}>
+            <input
+              type="text"
+              name="textbox"
+              placeholder="평가를 입력해주세요..."
+              ref={inputRef}
+            />
             <button className={styles.submitButton} onClick={onSubmit}>
-              제출
+              제출하기
             </button>
-          </footer>
+          </div>
         </section>
       ) : (
         <div className={styles.ratingIcon}>
           <img
             className={styles.ratingIconImg}
             src="/images/logo/hola_default.png"
-            alt="default loading spinner"
+            alt="default"
             onClick={handleClick}
           />
         </div>
