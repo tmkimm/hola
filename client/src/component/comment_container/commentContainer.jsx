7483beaeac724e99dcb4f17cf884484e2210c94f
Sihyun <sihyun930222@gmail.com>
2021-08-06 19:51:52 +0900

    feat: 댓글 입력시 로그인 검증 로직 추가
    댓글 입력시 로그인이 안된 유저는 로그인 창을 띄워주는 로직을 추가하였습니다.

--- client/src/component/comment_container/commentContainer.jsx
+++ client/src/component/comment_container/commentContainer.jsx
@@ -2,14 +2,35 @@ import React, { useEffect, useState } from "react";
 import CommentInput from "../comment_input/commentInput";
 import CommentList from "../comment_list/commentList";
 import studyService from "../../service/study_service";
+import { useDispatch, useSelector } from "react-redux";
+import Modal from "../modal/modal_component/modal";
+import LoginModal from "../modal/login_modal/loginModal";
+import { setModalVisible } from "../../store/loginStep";
 
 const CommentContainer = ({ id }) => {
   const [commentList, setCommentList] = useState([]);
   const [content, setContent] = useState("");
   const [isComplete, setIsComplete] = useState(false); // useEffect 발생용 state
+  const userId = useSelector((state) => state.user.id);
+  const modalVisible = useSelector((state) => state.loginStep.modalVisible);
+  const dispatch = useDispatch();
+
+  const openModal = () => {
+    document.body.style.overflow = "hidden";
+    dispatch(setModalVisible(true));
+  };
+
+  const closeModal = () => {
+    document.body.style.overflow = "auto";
+    dispatch(setModalVisible(false));
+  };
 
   // 댓글 등록 버튼
   const onRegisterClick = async (e) => {
+    if (userId === undefined) {
+      openModal();
+      return;
+    }
     await studyService.registerComment({ id, content });
     setContent("");
     setIsComplete((isComplete) => !isComplete);
@@ -34,6 +55,9 @@ const CommentContainer = ({ id }) => {
         setIsComplete={setIsComplete}
         isComplete={isComplete}
       ></CommentList>
+      <Modal visible={modalVisible} name="login" onClose={closeModal}>
+        <LoginModal handleClose={closeModal} tabIndex={0}></LoginModal>
+      </Modal>
     </>
   );
 };
