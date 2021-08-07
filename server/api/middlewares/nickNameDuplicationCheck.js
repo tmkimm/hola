5b5e6c815895d724334a295e57830e7d8dde4dc7
tmkimm <xoals1417@gmail.com>
2021-08-08 00:29:11 +0900

    fix: 회원 정보 수정 시 닉네임 중복체크 안되는 오류 수정

--- server/api/middlewares/nickNameDuplicationCheck.js
+++ server/api/middlewares/nickNameDuplicationCheck.js
@@ -3,21 +3,20 @@ import { asyncErrorWrapper } from '../../asyncErrorWrapper.js';
 
 // 회원 정보 수정 시 닉네임이 중복되었는지 체크한다.
 const nickNameDuplicationCheck = asyncErrorWrapper(async (req, res, next) => {
-    let { nickName } = req.query;
-    try {
-        if(nickName) {
-            const user = await User.findByNickName(nickName);
-            if(user) {
-                return res.status(200).json({
-                    message : `Nickname is duplicated.`,
-                    isExists: true
-                });
-            }
+    let nickName = req.query.nickName || req.body.nickName;
+    if(nickName) {
+        const user = await User.findByNickName(nickName);
+        if(user) {
+            return res.status(200).json({
+                message : `Nickname is duplicated.`,
+                isExists: true
+            });
         }
         next();
-    } catch(err) {
+    }
+    else {
         return res.status(200).json({
-            message : `Nickname is duplicated.`,
+            message : `Nickname not found.`,
             isExists: true
         });
     }
