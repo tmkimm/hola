9b4c43097e356f73daec9b4e59fc7af21ee7642a
Sihyun <sihyun930222@gmail.com>
2022-03-10 00:23:50 +0900

    refactor: 사용하지 않는 state 제거

--- client/src/component/study_item/studyItem.jsx
+++ client/src/component/study_item/studyItem.jsx
@@ -17,65 +17,48 @@ const StudyItem = ({ study, lastStudyElementRef }) => {
     else studyLang.push(study.language[i]);
   }
 
-  const [modalVisible, setModalVisible] = useState(false);
-
   const onClick = () => {
     history.push(`/study/${study._id}`);
   };
-  const closeModal = () => {
-    document.body.style.overflow = "auto";
-    setModalVisible(false);
-  };
 
   return (
-    <>
-      <li
-        ref={lastStudyElementRef ? lastStudyElementRef : null}
-        className={`${styles.studyItem} ${displayType}`}
-        onClick={onClick}
-      >
-        <div className={styles.schedule}>
-          <p className={styles.scheduleTitle}>일정 |</p>
-          <p classname={styles.scheduleInfo}>2020.02.05-03.14</p>
-        </div>
-        <h1 className={styles.title}>{study.title}</h1>
-        <p className={styles.hashtag}>#웹토이 프로젝트 #개발자 #기획자</p>
-        <ul className={styles.content}>
-          {studyLang.map((lang, i) => (
-            <li key={i} className={styles.language}>
-              <img
-                className={styles.languageImage}
-                src={`/images/languages/${lang}.svg`}
-                alt="language"
-              />
-            </li>
-          ))}
-        </ul>
-        <section className={styles.info}>
-          <Avatar size="small" userName="testUser"></Avatar>
-          <div className={styles.viewsAndComment}>
-            <div className={styles.infoItem}>
-              <FaRegEye size={24} color={"#9A9A9A"} />
-              <p className={styles.views}>{study.views}</p>
-            </div>
-            <div className={styles.infoItem}>
-              <FaRegCommentDots size={20} color={"#9A9A9A"} />
-              <p className={styles.comments}>{study.totalComments}</p>
-            </div>
+    <li
+      ref={lastStudyElementRef ? lastStudyElementRef : null}
+      className={`${styles.studyItem} ${displayType}`}
+      onClick={onClick}
+    >
+      <div className={styles.schedule}>
+        <p className={styles.scheduleTitle}>일정 |</p>
+        <p classname={styles.scheduleInfo}>2020.02.05-03.14</p>
+      </div>
+      <h1 className={styles.title}>{study.title}</h1>
+      <p className={styles.hashtag}>#웹토이 프로젝트 #개발자 #기획자</p>
+      <ul className={styles.content}>
+        {studyLang.map((lang, i) => (
+          <li key={i} className={styles.language}>
+            <img
+              className={styles.languageImage}
+              src={`/images/languages/${lang}.svg`}
+              alt="language"
+            />
+          </li>
+        ))}
+      </ul>
+      <section className={styles.info}>
+        <Avatar size="small" userName="testUser"></Avatar>
+        <div className={styles.viewsAndComment}>
+          <div className={styles.infoItem}>
+            <FaRegEye size={24} color={"#9A9A9A"} />
+            <p className={styles.views}>{study.views}</p>
           </div>
-        </section>
-        {study.isClosed && <div className={styles.closeNotice}>모집 완료</div>}
-      </li>
-      {modalVisible && (
-        <Modal visible={modalVisible} onClose={closeModal}>
-          <PostModal
-            study={study}
-            handleClose={closeModal}
-            tabIndex={0}
-          ></PostModal>
-        </Modal>
-      )}
-    </>
+          <div className={styles.infoItem}>
+            <FaRegCommentDots size={20} color={"#9A9A9A"} />
+            <p className={styles.comments}>{study.totalComments}</p>
+          </div>
+        </div>
+      </section>
+      {study.isClosed && <div className={styles.closeNotice}>모집 완료</div>}
+    </li>
   );
 };
 
