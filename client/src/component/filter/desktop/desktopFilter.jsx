94516073f9b265d88966ae8cfe1ce89f6120c5bb
Sihyun <sihyun930222@gmail.com>
2022-04-30 01:57:27 +0900

    feat: desktopFilter logic 추가 반영

--- client/src/component/filter/desktop/desktopFilter.jsx
+++ client/src/component/filter/desktop/desktopFilter.jsx
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './desktopFilter.module.css';
+import {
+  addLanguage,
+  clearLanguage,
+  initLanguage,
+  removeLanguage,
+  changeSubject,
+} from 'store/language';
 
 export const DesktopFilter = React.memo(() => {
+  const { subject, selected } = useSelector((state) => state.language);
+  console.log(subject, selected);
+  const dispatch = useDispatch();
+  const onIconClick = useCallback(
+    (Langauge, isSelected) => {
+      if (selected.length === 14) {
+        dispatch(clearLanguage());
+        dispatch(addLanguage(Langauge));
+      } else if (selected.length === 1) {
+        if (!isSelected) dispatch(addLanguage(Langauge));
+        else dispatch(initLanguage());
+      } else {
+        if (!isSelected) dispatch(addLanguage(Langauge));
+        else dispatch(removeLanguage(Langauge));
+      }
+    },
+    [dispatch, selected.length],
+  );
+  const onDeleteIconClick = (selected) => {
+    dispatch(removeLanguage(selected));
+  };
+  const onSubjectClick = (curSubject) => {
+    dispatch(changeSubject(curSubject));
+  };
+
+  const resetFilterClick = () => {
+    dispatch(clearLanguage());
+  };
   const languages = {
-    popular: [
-      'Javascript',
-      'Typescript',
-      'React',
-      'Vue',
-      'Svelte',
-      'Nextjs',
-      'Nodejs',
-      'Java',
-      'Spring',
-      'Go',
-      'Nestjs',
+    인기: [
+      'javascript',
+      'typescript',
+      'react',
+      'vue',
+      'svelte',
+      'nextjs',
+      'nodejs',
+      'java',
+      'spring',
+      'go',
+      'nestjs',
     ],
-    frontEnd: ['javascript', 'typescript', 'react', 'vue', 'svelte', 'nextjs'],
-    backEnd: [
+    프론트엔드: ['javascript', 'typescript', 'react', 'vue', 'svelte', 'nextjs'],
+    백엔드: [
       'java',
       'spring',
       'nodejs',
@@ -36,50 +72,67 @@ export const DesktopFilter = React.memo(() => {
       'graphql',
       'firebase',
     ],
-    mobile: ['flutter', 'swift', 'reactnative', 'unity'],
-    etc: ['aws', 'kubernetes', 'docker', 'git', 'figma', 'zeplin', 'jest'],
-    all: [],
+    모바일: ['flutter', 'swift', 'reactnative', 'unity'],
+    기타: ['aws', 'kubernetes', 'docker', 'git', 'figma', 'zeplin', 'jest'],
+    모두보기: [],
   };
 
   const subjects = ['인기', '프론트엔드', '모바일', '기타', '모두보기'];
-  const seletedLanguages = ['Javascript', 'Typescript'];
-  const displayType = true; //selected === true ? styles.full : styles.transparent;
 
   return (
     <section className={styles.filterWrapper}>
       <ul className={styles.subjects}>
-        {subjects.map((subject) => (
-          <li className={styles.subjectItem}>{subject}</li>
+        {subjects.map((curSubject) => (
+          <li
+            className={`${styles.subjectItem} ${
+              curSubject === subject ? styles.selectedSubject : ''
+            }`}
+            onClick={() => {
+              onSubjectClick(curSubject);
+            }}
+          >
+            {curSubject}
+          </li>
         ))}
       </ul>
       <ul className={styles.languages}>
-        {languages.popular.map((language) => (
-          <div className={styles.languageIcon}>
-            <img
-              className={`${styles.logo} ${displayType}`}
-              //onClick={() => onItemClick(language, selected)}
-              src={`/images/languages/${language}.svg`}
-              alt={language}
-            />
-            <span className={styles.languageName}>{language}</span>
-          </div>
-        ))}
+        {languages[subject].map((language) => {
+          const isSelected = selected.includes(language);
+          return (
+            <div
+              className={`${styles.languageIcon} ${
+                !selected.length || isSelected === true ? styles.full : styles.transparent
+              }`}
+              onClick={() => onIconClick(language, isSelected)}
+            >
+              <img
+                className={styles.logo}
+                src={`/images/languages/${language}.svg`}
+                alt={language}
+              />
+              <span className={styles.languageName}>{language}</span>
+            </div>
+          );
+        })}
       </ul>
       <div className={styles.selectedWrapper}>
         <ul className={styles.selectedLanguages}>
-          {seletedLanguages.map((language) => (
-            <div className={styles.selectedLanguage}>
-              <li>{language}</li>
+          {selected.map((selected) => (
+            <div className={styles.selectedLanguage} onClick={() => onDeleteIconClick(selected)}>
+              <li>{selected}</li>
               <img
                 className={styles.deleteButton}
-                //onClick={() => onItemClick(language, selected)}
                 src={`/images/info/delete.svg`}
                 alt='deleteButton'
               />
             </div>
           ))}
+          {selected.length !== 0 && (
+            <span className={styles.resetFilter} onClick={resetFilterClick}>
+              필터 초기화
+            </span>
+          )}
         </ul>
-        <div className={styles.resetFilter}>필터 초기화</div>
       </div>
     </section>
   );
