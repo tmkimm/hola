5cea13a779ea55bb48340c13a97ace6ccc76b19f
Sihyun <sihyun930222@gmail.com>
2022-04-30 10:15:20 +0900

    refactor: main component 분리

---
+++ client/src/component/mainContent/mainContent.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import styles from './mainContent.module.css';
+import { AiFillFire } from 'react-icons/ai';
+import { FaCalendarCheck } from 'react-icons/fa';
+import ShowByDate from 'component/show_studies/show_by_date/showByDate';
+import ShowByViews from 'component/show_studies/show_by_views/showByViews';
+
+export const MainContent = () => {
+  const SHOW_BY_VIEWS = '-views';
+  const SHOW_BY_DATE = '-createdAt';
+  const ACTIVE = styles.active;
+  const INACTIVE = styles.inactive;
+  const [category, setCategory] = useState(SHOW_BY_DATE);
+  const [checked, setChecked] = useState(true);
+
+  const toggleCategory = (toggleTo) => {
+    if (category === toggleTo) return; // 바꾸려는 대상이 현재 상태와 같으면 return
+
+    if (category === SHOW_BY_VIEWS) setCategory(SHOW_BY_DATE);
+    else setCategory(SHOW_BY_VIEWS);
+  };
+
+  const handleSelect = (e) => {
+    setChecked((checked) => !checked);
+  };
+  return (
+    <main className={styles.main}>
+      <div className={styles.categoryWrapper}>
+        <section className={styles.category}>
+          <div
+            className={`${styles.category__item} ${category === SHOW_BY_DATE ? ACTIVE : INACTIVE}`}
+            onClick={() => toggleCategory(SHOW_BY_DATE)}
+          >
+            <FaCalendarCheck />
+            <span className={styles.text}>최신</span>
+          </div>
+
+          <div
+            className={`${styles.category__item} ${category === SHOW_BY_DATE ? INACTIVE : ACTIVE}`}
+            onClick={() => toggleCategory(SHOW_BY_VIEWS)}
+          >
+            <AiFillFire />
+            <span className={styles.text}>인기</span>
+          </div>
+        </section>
+        <div className={styles.selectWrapper} onClick={handleSelect}>
+          <input
+            className={styles.selectboxInput}
+            type='checkbox'
+            name='languageSelect'
+            value='마감 글 보기'
+            checked={checked ? 'checked' : ''}
+            readOnly
+          ></input>
+          <label htmlFor='languageSelect'>
+            <span className={styles.selectTitle}>모집 중인 글만 보기</span>
+          </label>
+        </div>
+      </div>
+      {category === SHOW_BY_DATE ? (
+        <ShowByDate checked={checked} />
+      ) : (
+        <ShowByViews checked={checked} />
+      )}
+    </main>
+  );
+};
