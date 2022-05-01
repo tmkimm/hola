a2719f4214432f2862ff04d560811649909d25d2
Sihyun <sihyun930222@gmail.com>
2022-05-01 13:40:33 +0900

    chore: 미사용 주석 제거

--- client/src/component/write_button/writebuttonContainer.jsx
+++ client/src/component/write_button/writebuttonContainer.jsx
@@ -12,11 +12,8 @@ Writebutton을 감싸고 있는 component입니다.
 post 등록이 성공하면 main page로 이동하고,
 실패하면 error를 보여줍니다.
 
-To-do
--> modal 보여지도록 글 id return 받기
--> 실패했을때 에러 띄워주기
-
 */
+
 const WritebuttonContainer = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
