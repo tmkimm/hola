550e8583547043cf5f0c9fe17db6792641c68072
Sihyun <sihyun930222@gmail.com>
2022-06-27 00:33:52 +0900

    feat: 모바일 studylist style 수정

--- client/src/component/study_list/studyList.jsx
+++ client/src/component/study_list/studyList.jsx
@@ -3,10 +3,10 @@ import StudyItem from 'component/study_item/studyItem';
 import styles from './studyList.module.css';
 
 /* StudyList component는 map을 통해 StudyItem component를 생성합니다. */
-const StudyList = ({ studyList, lastStudyElementRef }) => {
+const StudyList = ({ studyList }) => {
   return (
     <ul className={styles.studyList}>
-      {studyList.map((study, index) => {
+      {studyList.map((study) => {
         return <StudyItem study={study} key={study._id}></StudyItem>;
       })}
     </ul>
