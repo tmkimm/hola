64712e863f4e3f5893eee106c7e578d19d1409a3
Sihyun <sihyun930222@gmail.com>
2022-07-07 21:58:00 +0900

    refactor: study item link style 반영

--- client/src/component/study_item/studyItem.jsx
+++ client/src/component/study_item/studyItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './studyItem.module.css';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { FaRegComment } from 'react-icons/fa';
 import { AiOutlineEye } from 'react-icons/ai';
 import { Avatar } from 'component/common/avatar';
@@ -22,43 +22,49 @@ const StudyItem = ({ study }) => {
 
   return (
     <li className={`${styles.studyItem} ${displayType}`} onClick={onClick}>
-      <div className={styles.schedule}>
-        <p className={styles.scheduleTitle}>시작 예정일 |</p>
-        <p className={styles.scheduleInfo}>{formatDate(study.startDate)}</p>
-      </div>
-      <h1 className={styles.title}>{study.title}</h1>
-      <ul className={styles.hashtag}>
-        {study.hashTag.map((hashtag, idx) => (
-          <li key={idx} className={styles.hashtagList}>
-            #{hashtag}
-          </li>
-        ))}
-      </ul>
-      <ul className={styles.content}>
-        {studyLang.map((lang, i) => (
-          <li key={i} className={styles.language}>
-            <img
-              className={styles.languageImage}
-              src={`/images/languages/${lang}.svg`}
-              alt='language'
-            />
-          </li>
-        ))}
-      </ul>
-      <section className={styles.info}>
-        <Avatar size='small' userName={study.author.nickName} imgPath={study.author.image}></Avatar>
-        <div className={styles.viewsAndComment}>
-          <div className={styles.infoItem}>
-            <AiOutlineEye size={28} color={'#999999'} />
-            <p className={styles.views}>{study.views}</p>
-          </div>
-          <div className={styles.infoItem}>
-            <FaRegComment size={20} color={'#999999'} />
-            <p className={styles.comments}>{study.totalComments}</p>
-          </div>
+      <Link to={`/study/${study._id}`}>
+        <div className={styles.schedule}>
+          <p className={styles.scheduleTitle}>시작 예정일 |</p>
+          <p className={styles.scheduleInfo}>{formatDate(study.startDate)}</p>
         </div>
-      </section>
-      {study.isClosed && <div className={styles.closeNotice}>모집 마감</div>}
+        <h1 className={styles.title}>{study.title}</h1>
+        <ul className={styles.hashtag}>
+          {study.hashTag.map((hashtag, idx) => (
+            <li key={idx} className={styles.hashtagList}>
+              #{hashtag}
+            </li>
+          ))}
+        </ul>
+        <ul className={styles.content}>
+          {studyLang.map((lang, i) => (
+            <li key={i} className={styles.language}>
+              <img
+                className={styles.languageImage}
+                src={`/images/languages/${lang}.svg`}
+                alt='language'
+              />
+            </li>
+          ))}
+        </ul>
+        <section className={styles.info}>
+          <Avatar
+            size='small'
+            userName={study.author.nickName}
+            imgPath={study.author.image}
+          ></Avatar>
+          <div className={styles.viewsAndComment}>
+            <div className={styles.infoItem}>
+              <AiOutlineEye size={28} color={'#999999'} />
+              <p className={styles.views}>{study.views}</p>
+            </div>
+            <div className={styles.infoItem}>
+              <FaRegComment size={20} color={'#999999'} />
+              <p className={styles.comments}>{study.totalComments}</p>
+            </div>
+          </div>
+        </section>
+        {study.isClosed && <div className={styles.closeNotice}>모집 마감</div>}
+      </Link>
     </li>
   );
 };
