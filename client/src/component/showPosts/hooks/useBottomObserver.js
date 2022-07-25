69ee047c04d3dc68307ece61445c7e09223b7ec0
siihyun <sihyun930222@gmail.com>
2022-07-25 23:49:30 +0900

    refactor: infinite scroll 임계치 수정

--- client/src/component/showPosts/hooks/useBottomObserver.js
+++ client/src/component/showPosts/hooks/useBottomObserver.js
@@ -4,8 +4,8 @@ export const useBottomObserver = ({
   target, // 감지할 대상, ref를 넘길 예정
   onIntersect, // 감지 시 실행할 callback 함수
   root = null, // 교차할 부모 요소, 아무것도 넘기지 않으면 document가 기본이다.
-  rootMargin = '0px', // root와 target이 감지하는 여백의 거리
-  threshold = 0.2, // 임계점. 1.0이면 root내에서 target이 100% 보여질 때 callback이 실행된다.
+  rootMargin = '50%', // root와 target이 감지하는 여백의 거리
+  threshold = 0.1, // 임계점. 1.0이면 root내에서 target이 100% 보여질 때 callback이 실행된다.
   hasNextPage,
   isFetching,
 }) => {
