2d778d37e3b2a3dc90c168c369e1bfbbc68d37eb
Sihyun <sihyun930222@gmail.com>
2022-10-23 10:01:21 +0900

    feat: 모집글 첫째 이미지 배너 반영

--- client/src/component/study_list/studyList.jsx
+++ client/src/component/study_list/studyList.jsx
@@ -6,6 +6,19 @@ import styles from './studyList.module.css';
 const StudyList = ({ studyList }) => {
   return (
     <ul className={styles.studyList}>
+      <a
+        className={styles.link}
+        href='https://docs.google.com/forms/d/e/1FAIpQLSe0Rzmm1oZLgYTdk2REB0E-Q-fZS5LUNXVFaJmsSjczt_kCbA/formResponse'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <img
+          className={styles.bannerImg}
+          title={'Event Banner'}
+          src={'/images/banner/bannerBlock.png'}
+          alt='Event Banner'
+        />
+      </a>
       {studyList.map((study) => {
         return <StudyItem study={study} key={study._id}></StudyItem>;
       })}
