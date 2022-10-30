ed3b78d3513932b299df626560e784411776984e
Sihyun <sihyun930222@gmail.com>
2022-10-30 19:58:36 +0900

    feat: 직군 필터 react-select cutstom

--- client/src/component/mainContent/mainContent.jsx
+++ client/src/component/mainContent/mainContent.jsx
@@ -6,6 +6,51 @@ import { ToggleSwitch } from 'component/toggleSwitch';
 import { useDispatch, useSelector } from 'react-redux';
 import { update } from 'store/study';
 import { AllIcon } from 'common/Icons/allIcon';
+import Select from 'react-select';
+
+const customStyles = {
+  control: (base, state) => ({
+    ...base,
+    width: '190px',
+    height: '42px',
+    background: 'white',
+    border: '1.5px solid #E3E3E3',
+    borderRadius: '36px',
+    boxShadow: state.isFocused ? null : null,
+  }),
+  indicatorSeparator: (base) => ({ ...base, display: 'none' }),
+  valueContainer: (provided, state) => ({
+    ...provided,
+    height: '42px',
+    padding: '7px 15px',
+    fontWeight: '700',
+    fontSize: '20px',
+    letterSpacing: '0.03em',
+  }),
+};
+
+const options = [
+  {
+    label: 'option 1',
+    value: 1,
+  },
+  {
+    label: 'option 2',
+    value: 2,
+  },
+  {
+    label: 'option 3',
+    value: 3,
+  },
+  {
+    label: 'option 4',
+    value: 4,
+  },
+  {
+    label: 'option 5',
+    value: 5,
+  },
+];
 
 export const MainContent = () => {
   const ALL = 'all';
@@ -67,6 +112,8 @@ export const MainContent = () => {
             <span className={styles.text}>스터디</span>
           </div>
         </section>
+
+        <Select placeholder={'내 포지션 찾기'} styles={customStyles} options={options} />
 
         <ToggleSwitch checked={checked} handleSelect={handleSelect} />
       </div>
