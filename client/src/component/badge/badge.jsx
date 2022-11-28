09fda4ea91a900a8f93afbf1ed56b932e061f789
Sihyun <sihyun930222@gmail.com>
2022-11-28 21:05:46 +0900

    feat: badge type 추가

--- client/src/component/badge/badge.jsx
+++ client/src/component/badge/badge.jsx
@@ -4,21 +4,22 @@ import styles from './badge.module.css';
 const getClass = (state) => {
   if (state === 'new') return styles.new;
   else if (state === 'deadline') return styles.deadline;
+  else if (state === 'project' || state === 'study') return styles.study;
   return styles.hot;
 };
 
 const Badge = ({ state }) => {
-  const badgeClass = styles.new; //`${styles}.${state}`;
-  console.log(badgeClass);
   const studyState = {
     new: '🍞 따끈따끈 새 글',
     deadline: '🔥 마감코앞',
     hot: '💙 인기',
+    study: '✏️ 스터디',
+    project: '📁 프로젝트',
   };
 
   return (
     <div className={`${styles.badge}`}>
-      <span className={getClass(state)}>{studyState[state]}</span>
+      {state ? <span className={getClass(state)}>{studyState[state]}</span> : null}
     </div>
   );
 };
