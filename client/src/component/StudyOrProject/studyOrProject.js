d7c996df4199037304e7917e533a5d12504a7216
Sihyun <sihyun930222@gmail.com>
2023-03-09 23:53:45 +0900

    refactor: StudyOrProject 컴포넌트 추출

---
+++ client/src/component/StudyOrProject/studyOrProject.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styles from './studyOrProject.module.css';
+import { StudyIcon, ProjectIcon } from 'common/Icons';
+import { AllIcon } from 'common/Icons/allIcon';
+
+const StudyOrProject = ({ category, toggleCategory }) => {
+  const ALL = 'all';
+  const PROJECT = 'project';
+  const STUDY = 'study';
+
+  const active = {
+    className: styles.active,
+    color: '#333333',
+  };
+  const inActive = {
+    className: styles.inactive,
+    color: '#858E86',
+  };
+
+  return (
+    <section className={styles.category}>
+      <div
+        className={`${styles.category__item} ${
+          category === ALL ? active.className : inActive.className
+        }`}
+        onClick={() => toggleCategory(ALL)}
+      >
+        <AllIcon stroke={category === ALL ? active.color : inActive.color} />
+        <span className={styles.text}>전체</span>
+      </div>
+      <div
+        className={`${styles.category__item} ${
+          category === PROJECT ? active.className : inActive.className
+        }`}
+        onClick={() => toggleCategory(PROJECT)}
+      >
+        <ProjectIcon stroke={category === PROJECT ? active.color : inActive.color} />
+        <span className={styles.text}>프로젝트</span>
+      </div>
+      <div
+        className={`${styles.category__item} ${
+          category === STUDY ? active.className : inActive.className
+        }`}
+        onClick={() => toggleCategory(STUDY)}
+      >
+        <StudyIcon stroke={category === STUDY ? active.color : inActive.color} />
+        <span className={styles.text}>스터디</span>
+      </div>
+    </section>
+  );
+};
+
+export default StudyOrProject;
--- client/src/component/mainContent/mainContent.jsx
+++ client/src/component/mainContent/mainContent.jsx
@@ -1,31 +1,18 @@
 import React, { useState } from 'react';
 import styles from './mainContent.module.css';
 import { Posts } from 'component/showPosts';
-import { StudyIcon, ProjectIcon } from 'common/Icons';
 import { ToggleSwitch } from 'component/toggleSwitch';
 import { useDispatch, useSelector } from 'react-redux';
 import { update } from 'store/study';
-import { AllIcon } from 'common/Icons/allIcon';
 import FindMyPosition from 'component/findMyPosition/findMyPosition';
+import StudyOrProject from 'component/StudyOrProject/studyOrProject';
 
+// API 정의, 전역 상태 정의, 호출까지
 export const MainContent = () => {
-  const ALL = 'all';
-  const PROJECT = 'project';
-  const STUDY = 'study';
-
-  const active = {
-    className: styles.active,
-    color: '#333333',
-  };
-  const inActive = {
-    className: styles.inactive,
-    color: '#858E86',
-  };
-
-  const category = useSelector((state) => state.study.mode);
-  const dispatch = useDispatch();
-
   const [checked, setChecked] = useState(true);
+  const category = useSelector((state) => state.study.mode);
+
+  const dispatch = useDispatch();
 
   const toggleCategory = (toggleTo) => {
     if (category === toggleTo) return; // 바꾸려는 대상이 현재 상태와 같으면 return
@@ -39,37 +26,8 @@ export const MainContent = () => {
   return (
     <main className={styles.main}>
       <div className={styles.categoryWrapper}>
-        <section className={styles.category}>
-          <div
-            className={`${styles.category__item} ${
-              category === ALL ? active.className : inActive.className
-            }`}
-            onClick={() => toggleCategory(ALL)}
-          >
-            <AllIcon stroke={category === ALL ? active.color : inActive.color} />
-            <span className={styles.text}>전체</span>
-          </div>
-          <div
-            className={`${styles.category__item} ${
-              category === PROJECT ? active.className : inActive.className
-            }`}
-            onClick={() => toggleCategory(PROJECT)}
-          >
-            <ProjectIcon stroke={category === PROJECT ? active.color : inActive.color} />
-            <span className={styles.text}>프로젝트</span>
-          </div>
-          <div
-            className={`${styles.category__item} ${
-              category === STUDY ? active.className : inActive.className
-            }`}
-            onClick={() => toggleCategory(STUDY)}
-          >
-            <StudyIcon stroke={category === STUDY ? active.color : inActive.color} />
-            <span className={styles.text}>스터디</span>
-          </div>
-        </section>
+        <StudyOrProject category={category} toggleCategory={toggleCategory} />
         <FindMyPosition />
-
         <ToggleSwitch checked={checked} handleSelect={handleSelect} />
       </div>
       <div className={styles.appWrapper}>
