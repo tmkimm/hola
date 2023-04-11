ca53e8dc8e5fce42a8d7ffa81ec11b1dd525c6d7
Sihyun <sihyun930222@gmail.com>
2023-04-11 22:52:20 +0900

    feat: Contact Point 컴포넌트 수정

--- client/src/component/contactPoint/contactPoint.jsx
+++ client/src/component/contactPoint/contactPoint.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import styles from './contactPoint.module.css';
+import { toast } from 'react-toastify';
 
 export const ContactPoint = ({ contactPoint, contactType }) => {
+  const copyContent = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      toast.success('클립보드에 복사되었어요!', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  };
+
   if (contactType.value === 'ok' || contactType.value === 'gf') {
     return (
       <div className={styles.contactWrapper}>
@@ -12,6 +25,11 @@ export const ContactPoint = ({ contactPoint, contactType }) => {
       </div>
     );
   } else {
-    return <div className={styles.email}>{contactPoint}</div>;
+    return (
+      <div className={styles.email} onClick={() => copyContent(contactPoint)}>
+        <div>이메일</div>
+        <img className={styles.linkImg} src={'/images/info/link.svg'} alt='' />
+      </div>
+    );
   }
 };
