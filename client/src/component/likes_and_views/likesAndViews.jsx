bbe50e162115de127931756a3345f14d3c490960
Sihyun <sihyun930222@gmail.com>
2023-05-07 14:41:38 +0900

    feat: likeAndView styled-component 적용

--- client/src/component/likes_and_views/likesAndViews.jsx
+++ client/src/component/likes_and_views/likesAndViews.jsx
@@ -4,25 +4,19 @@ import studyService from 'service/study_service';
 import { setModalVisible } from 'store/loginStep';
 import LoginModal from 'component/modal/login_modal/loginModal';
 import Modal from 'component/modal/modal_component/modal';
-import styles from './likesAndViews.module.css';
-
-/* 
-
-좋아요수와 조회수를 보여주는 component입니다.
-
-*/
+import * as S from './styled';
 
 const LikesAndViews = ({ views, studyId, userId }) => {
-  const [likeImg, setLikeImg] = useState('heart_unfilled');
+  const [likeImg, setLikeImg] = useState('bookmark');
   const [totalLikes, setTotalLikes] = useState(0);
   useEffect(() => {
     studyService.getLikesUser(studyId).then((res) => {
       setTotalLikes(res.likeUsers.length);
       if (userId === undefined) {
-        setLikeImg('heart_unfilled');
+        setLikeImg('bookmark');
       } else {
         const isLike = res.likeUsers.filter((likeUserid) => likeUserid === userId);
-        isLike.length === 0 ? setLikeImg('heart_unfilled') : setLikeImg('heart_filled');
+        isLike.length === 0 ? setLikeImg('bookmark') : setLikeImg('bookmark_filled');
       }
     });
   }, [studyId, userId]);
@@ -47,34 +41,29 @@ const LikesAndViews = ({ views, studyId, userId }) => {
       return;
     }
 
-    if (likeImg === 'heart_filled') {
+    if (likeImg === 'bookmark') {
       const response = await studyService.deleteLikes(studyId);
-      setLikeImg('heart_unfilled');
+      setLikeImg('bookmark');
       setTotalLikes(response.data.likeUsers.length);
     } else {
       const response = await studyService.addLikes(studyId);
       setTotalLikes(response.data.likeUsers.length);
-      setLikeImg('heart_filled');
+      setLikeImg('bookmark_filled');
     }
   };
 
   return (
     <>
-      <section className={styles.likesAndViewsWrapper}>
-        <div className={styles.likes}>
-          <img
-            onClick={handleLikesClick}
-            className={styles.itemImg}
-            src={`/images/info/${likeImg}.png`}
-            alt='likes'
-          />
-          <p>{totalLikes}</p>
-        </div>
-        <div className={styles.views}>
-          <img className={styles.eyeImg} src='/images/info/eye.png' alt='views' />
-          <p>{views}</p>
-        </div>
-      </section>
+      <S.Container>
+        <S.Views>
+          <S.EyeImg src='/images/info/eye.png' alt='views' />
+          <S.Text>{views}</S.Text>
+        </S.Views>
+        <S.Likes>
+          <S.LikesImg onClick={handleLikesClick} src={`/images/info/${likeImg}.png`} alt='likes' />
+          <S.Text>{totalLikes}</S.Text>
+        </S.Likes>
+      </S.Container>
       <Modal visible={modalVisible} name='login' onClose={closeModal}>
         <LoginModal handleClose={closeModal} tabIndex={0}></LoginModal>
       </Modal>
