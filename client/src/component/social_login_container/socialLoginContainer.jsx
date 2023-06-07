a6419e313726dc6c630e9b9d28da5b15d0bb33cc
Sihyun <sihyun930222@gmail.com>
2023-06-08 01:51:38 +0900

    chore: 콘솔 제거

--- client/src/component/social_login_container/socialLoginContainer.jsx
+++ client/src/component/social_login_container/socialLoginContainer.jsx
@@ -42,7 +42,7 @@ const SocialLoginContainer = ({ handleClose }) => {
   };
 
   const kakaoOnFailure = (error) => {
-    console.log('error : ', error);
+    console.log(error);
   };
   return (
     <SocialLogin
