7c9e6112568482eb3a60d36a26d2121036e1aad6
Sihyun <sihyun930222@gmail.com>
2023-07-29 13:15:48 +0900

    feat: custom option 컴포넌트 추가

--- client/src/domains/myPage/component/customOption/index.js
+++ client/src/domains/myPage/component/customOption/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import * as S from './styled';
 
 const CustomOption = ({ innerProps, isDisabled, ...props }) => {
-  return !isDisabled ? <div {...innerProps}>{props.data.label}</div> : null;
+  const { label } = props;
+  const [firstLetter, ...rest] = label;
+
+  return !isDisabled ? (
+    <S.Container {...innerProps}>
+      <S.LogoImg src={`/images/logo/${firstLetter.toLowerCase()}${rest.join('')}.png`} />
+      <S.Label>{props.data.label}</S.Label>
+    </S.Container>
+  ) : null;
 };
 
 export default CustomOption;
