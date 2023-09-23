804dce53f59d10e3dcaad4e7fa21517b175c27af
Sihyun <sihyun930222@gmail.com>
2023-09-23 21:44:41 +0900

    feat: 공통 컴포넌트 반영 및 Navbar style 수정

--- client/src/page/main/main.jsx
+++ client/src/page/main/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from 'component/nav_bar/navbar';
 import Rating from 'component/rating/rating';
 import { MainContent } from 'domains/main/component/mainContent';
-import { Carousel } from 'domains/main/component/carousel';
+import { MainCarousel } from 'domains/main/component/mainCarousel';
 import TopButton from 'component/top_button/TopButton';
 import Footer from 'component/footer/footer';
 import Trending from 'domains/main/component/trending';
@@ -11,7 +11,7 @@ const Main = () => {
   return (
     <>
       <Navbar />
-      <Carousel />
+      <MainCarousel />
       <Trending />
       <MainContent />
       <TopButton />
