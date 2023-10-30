d6d5433be7384c7b992087ec1e54cf7b930de27f
Sihyun <sihyun930222@gmail.com>
2023-10-30 23:55:05 +0900

    feat: url sync 버그 수정

--- client/src/domains/eventPage/components/ItFilter/index.js
+++ client/src/domains/eventPage/components/ItFilter/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { stringify, parse } from 'qs';
@@ -18,20 +18,18 @@ const makeQueryString = (state) => {
 };
 
 const ItFilterView = ({ isMobile }) => {
-  const isMounted = useRef(false);
   const filterState = useSelector((state) => state.itFilter);
   const history = useHistory();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    //NOTE: 첫 마운트시 url을 sync하지 않습니다.
-    if (!isMounted.current) {
-      dispatch(updateField(parse(window.location.search.substring(1))));
-      isMounted.current = true;
-      return;
-    }
+    //NOTE: 첫 마운트시 Url과 전역 State를 Sync합니다.
+    dispatch(updateField(parse(window.location.search.substring(1))));
+  }, []);
+
+  useEffect(() => {
     history.push({ pathName: '/hola-it', search: makeQueryString(filterState) });
-  }, [filterState, history, dispatch]);
+  }, [filterState, history]);
 
   return isMobile ? <DesktopItFilter /> : <DesktopItFilter />;
 };
