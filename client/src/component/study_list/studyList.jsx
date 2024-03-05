94c612d70e7f7ea941a7c33692c0e2c7950d0b30
Sihyun <sihyun930222@gmail.com>
2024-03-06 00:59:53 +0900

    feat: ad 제거

--- client/src/component/study_list/studyList.jsx
+++ client/src/component/study_list/studyList.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
 import StudyItem from 'component/study_item/studyItem';
 import styles from './studyList.module.css';
-import { useMediaQuery } from 'react-responsive';
-import { useEventLog } from 'domains/main/hooks/useEventLog';
 
 /* StudyList component는 map을 통해 StudyItem component를 생성합니다. */
 const StudyList = ({ studyList, type }) => {
-  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
-  const { mutate } = useEventLog();
-
   return (
     <ul className={styles.studyList}>
-      {!isMobile && (
-        <img
-          className={styles.adBlockImg}
-          src='images/banner/ad-block-ll.png'
-          alt='ad-block'
-          onClick={() => {
-            mutate({ advertisementId: '65cb764fc9cb7c177c881ac1', logType: 'reach' });
-            window.open(
-              'https://techit.education/school/kdt-backendj-10th?utm_source=hola&utm_medium=vertical&utm_campaign=kdtbesj07_hola',
-              '_blank',
-            );
-          }}
-        />
-      )}
       {studyList.map((study) => {
         return <StudyItem study={study} type={type} key={study._id}></StudyItem>;
       })}
--- client/src/page/main/main.jsx
+++ client/src/page/main/main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from 'component/nav_bar/navbar';
 import Rating from 'component/rating/rating';
 import { MainContent } from 'domains/main/component/mainContent';
@@ -6,17 +6,8 @@ import { Carousel } from 'domains/main/component/mainCarousel';
 import TopButton from 'component/top_button/TopButton';
 import Footer from 'component/footer/footer';
 import Trending from 'domains/main/component/trending';
-import { useEventLog } from 'domains/main/hooks/useEventLog';
 
 const Main = () => {
-  const { mutate } = useEventLog();
-
-  useEffect(() => {
-    if (window.innerWidth < 767) return;
-    console.log('window.in', window.innerWidth);
-    mutate({ advertisementId: '65cb764fc9cb7c177c881ac1', logType: 'impression' });
-  }, []);
-
   return (
     <>
       <Navbar />
